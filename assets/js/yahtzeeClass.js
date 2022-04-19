4aa66380fd35da2076799114afcc0627d45ed30e
Keifer Rasner <rasnerkeifer@gmail.com>
2022-04-19 08:27:42 -0500

    Added Yahtzee class with empty methods I deem necessary

---
+++ assets/js/yahtzeeClass.js
@@ -0,0 +1,22 @@
+class Yahtzee {
+    constructor () {
+        this.score = [];
+        this.numberOfRolls = 0;
+    }
+
+    startGame () {}
+
+    rollDice () {}
+
+    createDiceImage () {}
+
+    reRollDice () {}
+
+    updateScore () {}
+
+    calculateFinalScore () {}
+
+    fullHouseChecker () {}
+
+    straightChecker () {}
+}
