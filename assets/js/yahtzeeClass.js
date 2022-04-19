1d058249a1d1099780c8995e4691f3e1699ec603
Keifer Rasner <rasnerkeifer@gmail.com>
2022-04-19 09:27:05 -0500

    Working through scoring, trips-yahtzee works fine, fullhouse works fine, straight checker works MOST of the time...

--- assets/js/yahtzeeClass.js
+++ assets/js/yahtzeeClass.js
@@ -26,7 +26,8 @@ class Yahtzee {
 
     // --- Need something to completely end the game --- //
     endGame () {
-
+        const result = this.calculateFinalScore();
+        console.log(result)
     }
 
     // --- Returns a random Die (number 1-6) --- //
@@ -60,7 +61,7 @@ class Yahtzee {
             d.classList.length === 1 ? d.remove() : this.score.push( Number(d.classList[0]) );
         })
 
-        this.updateScore()
+        // this.updateScore()
         let remainingDice = 5 - this.score.length;
         for (let i=0; i<remainingDice; i++) {
             let newDice = this.rollDice();
@@ -69,11 +70,82 @@ class Yahtzee {
         }
     }
 
+    // --- I might not actually need this ??? --- //
     updateScore () {}
 
-    calculateFinalScore () {}
+    calculateFinalScore () {
+        // --- Sort array in ascending order to make it easier to check for straights --- //
+        this.score.sort((a,b) => a - b)
+        
+        // --- Variables --- //
+        const sum = this.score.reduce((a,b) => a + b);
+        const findDuplicates = this.score.filter( (item, index) => this.score.indexOf(item) !== index );
+        const tripsThroughYahtzee = this.score.filter( num => num === findDuplicates[0] );
+        
+        // --- Logic --- //
+        // NEED to check for full house FIRST
+        if ( this.fullHouseChecker(this.score) ) return 'Full House';
+        // Check for Trips-Yahtzee
+        if (tripsThroughYahtzee.length > 2 ) return this.helper(tripsThroughYahtzee.length);
+        // Check for Straight
+        let straight = this.straightChecker(this.score);
+        if (straight) return straight; 
+        // default return sum
+        return sum;
+    
+    }
 
-    fullHouseChecker () {}
+    // --- Creates two arrays (arr1 = array that matches any item in passed arr that is equal to the value in the first index) (arr2 = same as arr1 but matches based off the last value) --- //
+    // --- returns true if one array.length = 2 & the other is = 3 (full house) --- //
+    fullHouseChecker (arr) {
+        let arr1 = arr.filter( num => num === arr[0] );
+        let arr2 = arr.filter( num => num === arr[4] );
+        return arr1.length === 2 && arr2.length === 3 || arr2.length === 2 && arr1.length === 3 ? true : false;
+    }
 
-    straightChecker () {}
+    // --- This is complicated and might need to be refactored --- //
+    straightChecker (arr) {
+        let incrementor = 0;
+        let straight = arr.filter( num => {
+            incrementor ++
+            return num === incrementor  
+        })
+    
+        if (straight.length === 4) return 'Small Straight'
+        else if (straight.length === 5) return 'Large Straight'
+        else {
+            let test1 = arr[0] - 1;
+            let incre = 0;
+            let test = arr.filter( num => {
+                incre ++
+                return test1 + incre === num;
+            })
+            if (test.length === 5) return "Large Straight";
+            let start = arr[1] - 1;
+            let temp = arr;
+            temp.shift()
+            let incrementor2 = 0;
+            let straight2 = temp.filter( num => {
+                incrementor2 ++
+                return start + incrementor2 === num;
+            })
+    
+            if (straight2.length === 4) return 'Small Straight'
+        }
+    }
+
+    helper (num) {
+        let result;
+        switch(num) {
+            case 3: 
+                result = 'Trips'
+                break;
+            case 4:
+                result = "Quads" 
+                break;
+            default: 
+                result = "Yahtzee"
+        }
+        return result;
+    }
 }
